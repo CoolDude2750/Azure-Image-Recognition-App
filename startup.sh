4301c00c3e80917a1d6fd455ce8b4f7c05b854df
Simon Peter Ssenjovu <simonssenjovu2527@gmail.com>
2026-06-05 10:15:58 +0100

    new implementation workflow

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
-gunicorn --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 app:app
+#!/usr/bin/env bash
+cd /home/site/wwwroot || exit 1
+exec gunicorn --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 src.api:app
